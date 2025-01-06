374c97c6eacab99d93a5161cb6cfc196cf2b737e
NymphaPamba <praisepamba@gmail.com>
2025-01-06 08:21:40 +0300

    initial commits

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        # Initialize title and page_count
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count  # This will trigger the setter
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        # Check if the page_count is an integer
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        # Simulate turning the page and print a message
+        print("Flipping the page...wow, you read fast!")
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        # Initialize brand and size
+        self.brand = brand
+        self._size = None
+        self.size = size  # This will trigger the setter
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        # Check if the size is an integer
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        # Repair the shoe and set the condition to 'New'
+        self.condition = "New"
+        print("Your shoe is as good as new!")
